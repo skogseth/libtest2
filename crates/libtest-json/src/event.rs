da6375c9cdbac53c1b580b7739e9f360e566cf66
Ed Page <eopage@gmail.com>
2025-03-28 15:41:43 -0500

    feat(json): Split out into its own crate

---
+++ crates/libtest-json/src/event.rs
@@ -0,0 +1,76 @@
+#[derive(Clone, Debug)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize))]
+#[cfg_attr(feature = "serde", serde(rename_all = "kebab-case"))]
+#[cfg_attr(feature = "serde", serde(tag = "event"))]
+pub enum Event {
+    DiscoverStart,
+    DiscoverCase {
+        name: String,
+        mode: RunMode,
+        run: bool,
+    },
+    DiscoverComplete {
+        #[allow(dead_code)]
+        elapsed_s: Elapsed,
+        seed: Option<u64>,
+    },
+    SuiteStart,
+    CaseStart {
+        name: String,
+    },
+    CaseComplete {
+        name: String,
+        #[allow(dead_code)]
+        mode: RunMode,
+        status: Option<RunStatus>,
+        message: Option<String>,
+        #[allow(dead_code)]
+        elapsed_s: Option<Elapsed>,
+    },
+    SuiteComplete {
+        elapsed_s: Elapsed,
+    },
+}
+
+#[derive(Copy, Clone, Default, Debug, PartialEq, Eq)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize))]
+#[cfg_attr(feature = "serde", serde(rename_all = "kebab-case"))]
+pub enum RunMode {
+    #[default]
+    Test,
+    Bench,
+}
+
+impl RunMode {
+    pub fn as_str(&self) -> &str {
+        match self {
+            Self::Test => "test",
+            Self::Bench => "bench",
+        }
+    }
+}
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize))]
+#[cfg_attr(feature = "serde", serde(rename_all = "kebab-case"))]
+pub enum RunStatus {
+    Ignored,
+    Failed,
+}
+
+#[derive(Copy, Clone, Default, Debug, PartialEq, Eq)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize))]
+#[cfg_attr(feature = "serde", serde(into = "String"))]
+pub struct Elapsed(pub std::time::Duration);
+
+impl std::fmt::Display for Elapsed {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:.3}s", self.0.as_secs_f64())
+    }
+}
+
+impl From<Elapsed> for String {
+    fn from(elapsed: Elapsed) -> Self {
+        elapsed.0.as_secs_f64().to_string()
+    }
+}
--- crates/libtest2-harness/src/notify/mod.rs
+++ crates/libtest2-harness/src/notify/mod.rs
@@ -28,79 +28,8 @@ pub(crate) trait Notifier {
     fn notify(&mut self, event: Event) -> std::io::Result<()>;
 }
 
-#[derive(Clone, Debug)]
-#[cfg_attr(feature = "json", derive(serde::Serialize))]
-#[cfg_attr(feature = "json", serde(rename_all = "kebab-case"))]
-#[cfg_attr(feature = "json", serde(tag = "event"))]
-pub(crate) enum Event {
-    DiscoverStart,
-    DiscoverCase {
-        name: String,
-        mode: RunMode,
-        run: bool,
-    },
-    DiscoverComplete {
-        #[allow(dead_code)]
-        elapsed_s: Elapsed,
-        seed: Option<u64>,
-    },
-    SuiteStart,
-    CaseStart {
-        name: String,
-    },
-    CaseComplete {
-        name: String,
-        #[allow(dead_code)]
-        mode: RunMode,
-        status: Option<RunStatus>,
-        message: Option<String>,
-        #[allow(dead_code)]
-        elapsed_s: Option<Elapsed>,
-    },
-    SuiteComplete {
-        elapsed_s: Elapsed,
-    },
-}
+pub(crate) use libtest_json::Elapsed;
+pub(crate) use libtest_json::Event;
+pub(crate) use libtest_json::RunStatus;
 
-#[derive(Copy, Clone, Default, Debug, PartialEq, Eq)]
-#[cfg_attr(feature = "json", derive(serde::Serialize))]
-#[cfg_attr(feature = "json", serde(rename_all = "kebab-case"))]
-pub enum RunMode {
-    #[default]
-    Test,
-    Bench,
-}
-
-impl RunMode {
-    pub(crate) fn as_str(&self) -> &str {
-        match self {
-            Self::Test => "test",
-            Self::Bench => "bench",
-        }
-    }
-}
-
-#[derive(Copy, Clone, Debug, PartialEq, Eq)]
-#[cfg_attr(feature = "json", derive(serde::Serialize))]
-#[cfg_attr(feature = "json", serde(rename_all = "kebab-case"))]
-pub(crate) enum RunStatus {
-    Ignored,
-    Failed,
-}
-
-#[derive(Copy, Clone, Default, Debug, PartialEq, Eq)]
-#[cfg_attr(feature = "json", derive(serde::Serialize))]
-#[cfg_attr(feature = "json", serde(into = "String"))]
-pub(crate) struct Elapsed(pub std::time::Duration);
-
-impl std::fmt::Display for Elapsed {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{:.3}s", self.0.as_secs_f64())
-    }
-}
-
-impl From<Elapsed> for String {
-    fn from(elapsed: Elapsed) -> Self {
-        elapsed.0.as_secs_f64().to_string()
-    }
-}
+pub use libtest_json::RunMode;
