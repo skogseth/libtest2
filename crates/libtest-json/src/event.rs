de9ada2ed824b22d58a99a98e48864ac842802af
Ed Page <eopage@gmail.com>
2025-07-21 15:27:24 -0500

    feat(json): Allow deserializing libtest json output

--- crates/libtest-json/src/event.rs
+++ crates/libtest-json/src/event.rs
@@ -1,6 +1,6 @@
 #[derive(Clone, Debug)]
 #[cfg_attr(feature = "unstable-schema", derive(schemars::JsonSchema))]
-#[cfg_attr(feature = "serde", derive(serde::Serialize))]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 #[cfg_attr(feature = "serde", serde(rename_all = "kebab-case"))]
 #[cfg_attr(feature = "serde", serde(tag = "event"))]
 pub enum Event {
@@ -38,7 +38,7 @@ impl Event {
 
 #[derive(Copy, Clone, Default, Debug, PartialEq, Eq)]
 #[cfg_attr(feature = "unstable-schema", derive(schemars::JsonSchema))]
-#[cfg_attr(feature = "serde", derive(serde::Serialize))]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 #[cfg_attr(feature = "serde", serde(rename_all = "kebab-case"))]
 pub enum RunMode {
     #[default]
@@ -57,7 +57,7 @@ impl RunMode {
 
 #[derive(Copy, Clone, Debug, PartialEq, Eq)]
 #[cfg_attr(feature = "unstable-schema", derive(schemars::JsonSchema))]
-#[cfg_attr(feature = "serde", derive(serde::Serialize))]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 #[cfg_attr(feature = "serde", serde(rename_all = "kebab-case"))]
 pub enum RunStatus {
     Ignored,
@@ -66,13 +66,31 @@ pub enum RunStatus {
 
 #[derive(Copy, Clone, Default, Debug, PartialEq, Eq)]
 #[cfg_attr(feature = "unstable-schema", derive(schemars::JsonSchema))]
-#[cfg_attr(feature = "serde", derive(serde::Serialize))]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 #[cfg_attr(feature = "serde", serde(into = "String"))]
+#[cfg_attr(feature = "serde", serde(try_from = "String"))]
 pub struct Elapsed(pub std::time::Duration);
 
 impl std::fmt::Display for Elapsed {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "{:.3}s", self.0.as_secs_f64())
+    }
+}
+
+impl std::str::FromStr for Elapsed {
+    type Err = std::num::ParseFloatError;
+
+    fn from_str(src: &str) -> Result<Self, Self::Err> {
+        let secs = src.parse()?;
+        Ok(Elapsed(std::time::Duration::from_secs_f64(secs)))
+    }
+}
+
+impl TryFrom<String> for Elapsed {
+    type Error = std::num::ParseFloatError;
+
+    fn try_from(inner: String) -> Result<Self, Self::Error> {
+        inner.parse()
     }
 }
 
