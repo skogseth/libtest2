e3a14b887e2d6f6ee7fe5f5eb3ccf1fb19dfda45
Herman Skogseth <herman.skogseth@me.com>
2025-10-20 10:50:45 +0200

    Custom lists of tests

--- crates/libtest2/src/macros.rs
+++ crates/libtest2/src/macros.rs
@@ -15,6 +15,33 @@ macro_rules! _main_parse {
 #[macro_export]
 #[allow(clippy::crate_in_macro_def)] // accessing item defined by `_main_parse`
 macro_rules! _test_parse {
+    (#[test($tag:ident)] fn $name:ident $($item:tt)*) => {
+        #[allow(non_camel_case_types)]
+        struct $name;
+
+        impl $crate::_private::Case for $name {
+            fn name(&self) -> &str {
+                $crate::_private::push!($tag, _: $crate::_private::DynCase = $crate::_private::DynCase(&$name));
+
+                stringify!($name)
+            }
+            fn kind(&self) -> $crate::_private::TestKind {
+                Default::default()
+            }
+            fn source(&self) -> Option<&$crate::_private::Source> {
+                None
+            }
+            fn exclusive(&self, _: &$crate::TestContext) -> bool {
+                false
+            }
+
+            fn run(&self, context: &$crate::TestContext) -> $crate::RunResult {
+                fn run $($item)*
+
+                run(context)
+            }
+        }
+    };
     (#[test] fn $name:ident $($item:tt)*) => {
         #[allow(non_camel_case_types)]
         struct $name;
