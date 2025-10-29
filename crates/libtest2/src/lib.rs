e313c58b168c7056971ac24e4e141927e5e3b981
Ed Page <eopage@gmail.com>
2025-10-29 13:57:37 -0500

    docs(test2): Cover ignore macro differences

--- crates/libtest2/src/lib.rs
+++ crates/libtest2/src/lib.rs
@@ -37,6 +37,7 @@
 //!
 //! - `#[test]` does not support all `Termination` types as return values,
 //!   only what [`IntoRunResult`] supports.
+//! - `#[ignore]` must come after the `#[test]` macro
 //! - Output capture and `--no-capture`: simply not supported. The official
 //!   `libtest` uses internal `std` functions to temporarily redirect output.
 //!   `libtest` cannot use those, see also [libtest2#12](https://github.com/assert-rs/libtest2/issues/12)
